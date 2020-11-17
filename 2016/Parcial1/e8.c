e680955c5abc4355e7197e89ddeab8b2042adf54
davibernardos <davibernardos@gmail.com>
2020-11-16 22:42:33 -0300

    teste commit

--- 2016/Parcial1/e8.c
+++ 2016/Parcial1/e8.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* aaaaa run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	char opcaoMenu;
